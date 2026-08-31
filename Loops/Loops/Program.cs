792457701d269da422fbd1640c3dc4dfa3658314
Felipe Costa <felipe.costa@hlss.com.br>
2026-08-31 20:46:05 -0300

    feat: implement inventory control loop exercise
    - Adds ControleDeEstoque method using a do-while loop. - Corrects variable scoping to properly accumulate total inventory across iterations. - Utilizes Thread.Sleep and Console.Clear to improve terminal UX before exiting.

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -4,12 +4,16 @@ do
     Console.WriteLine("Bem vindo aao menu de opções de exercicios!");
     Console.WriteLine("Entre com a opção do exercicio que você quer executar");
     Console.WriteLine("Exercicio 1: Soma das vendas");
+    Console.WriteLine("Exercicio 2: Controle de estoque");
     Console.WriteLine("Sair: -1");
     opcaoMenu = int.Parse(Console.ReadLine()!);
     switch (opcaoMenu)
     {
         case 1:
             SomaDasVendas();
+            break;
+        case 2:
+            ControleDeEstoque();
             break;
     }
 
@@ -21,6 +25,7 @@ void SomaDasVendas()
 {
     int encerraRegistros = 0;
     double totalVendas = 0.00;
+    Console.Clear();
     while (encerraRegistros != 1)
     {
         Console.WriteLine("Entre com o valor da venda:");
@@ -33,4 +38,30 @@ void SomaDasVendas()
     Console.ReadKey();
     Console.Clear();
 
+}
+void ControleDeEstoque()
+{
+    int controleLoop = 0;
+    int quantidadeEstoque = 0;
+    Console.Clear();
+    do
+    {
+        Console.WriteLine("Deseja adicionar um produto ao estoque?");
+        Console.WriteLine("1 - SIM | 2 - NÃO");
+        controleLoop = int.Parse(Console.ReadLine()!);
+        if (controleLoop == 1)
+        {
+            Console.WriteLine("Quantidade:");
+            quantidadeEstoque = quantidadeEstoque + int.Parse(Console.ReadLine()!);
+            Console.WriteLine($"Estoque atual: {quantidadeEstoque}");
+
+        }
+        else
+        {
+            Console.WriteLine("Obrigado por usar nosso sistema!");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    } while (controleLoop != 2);
+    
 }
